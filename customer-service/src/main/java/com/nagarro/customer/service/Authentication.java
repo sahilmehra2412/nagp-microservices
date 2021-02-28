74bb5923570b5d77ebf3bf2b1ddb4ae5b40c084b
Sahil Mehra <sahilmehra2412@gmail.com>
2021-02-28 22:15:46 +0530

    Added Database and admin-service

--- customer-service/src/main/java/com/nagarro/customer/service/Authentication.java
+++
@@ -1,9 +0,0 @@
-package com.nagarro.customer.service;
-
-import com.nagarro.customer.model.Users;
-
-public interface Authentication {
-	Users signup(String username, String password);
-	
-	boolean login(String username, String password);
-}
